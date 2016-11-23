f0ac212dfc785df811bbb6032f482171a1a18faa
Bryan Shelton <bryan@sheltonplace.com>
2016-11-23 13:07:34 -0800

    Use sha256

--- docker-credential-ecr-login.rb
+++ docker-credential-ecr-login.rb
@@ -1,7 +1,7 @@
 class DockerCredentialEcrLogin < Formula
   url "https://github.com/roverdotcom/homebrew-tap/raw/2f8cab835293944aec9c5297aad2350844264930/assets/docker-credential-ecr-login-0.0.1.tar.gz"
 
-  sha1 "92e063380348d1a48f589a86f391d47b42e125da"
+  sha256 "2ab67e189ce370686f61f0b455024be1db578c08c88e161cd7801eadd23a7812"
   version "0.0.1"
 
   def install
